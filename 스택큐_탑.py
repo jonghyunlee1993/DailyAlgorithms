8ce288685d8f004305e0dbcf85561ef46c8738cc
Jonghyun Lee <green198800@yonsei.ac.kr>
2020-01-14 08:31:39 +0900

    Add 스택/큐 탑 문제

---
+++ 스택큐_탑.py
@@ -0,0 +1,15 @@
+heights = [6, 9, 5, 7, 4]
+
+def solution(heights):
+	answer = [0] * len(heights)
+
+	for i in range(len(heights)-1, 0, -1):
+		for j in range(i - 1, -1, -1):
+			if heights[i] < heights[j]:
+				answer[i] = j + 1
+				break
+
+	return answer
+
+res = solution(heights)
+print(res)
