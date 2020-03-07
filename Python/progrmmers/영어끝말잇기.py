00d59454ca4796aac1556e4e9d1ff5d9b85d81d7
Jonghyun Lee <green198800@yonsei.ac.kr>
2020-03-07 21:40:34 +0900

    modify 영어끝말잇기

--- Python/progrmmers/영어끝말잇기.py
+++ Python/progrmmers/영어끝말잇기.py
@@ -1,3 +1,18 @@
+# 스택으로 변경
+
+def solution(n, words):
+    prev_word = words[0]
+    word_list = [prev_word]
+    
+    for i, word in enumerate(words[1:]):
+        if word in word_list or not prev_word[-1] == word[0]:
+            return [((i+1)%n)+1, ((i+1)//n)+1]
+    
+        word_list.append(word)
+        prev_word = word
+
+    return [0, 0]
+
 # 거의 다 풀었는데 마이너한 오류 있음
 from collections import Counter
 import math
@@ -14,3 +29,4 @@ def solution(n, words):
         return [n-(i+1)%n, (i+1)/n]
     
     return [0, 0]
+
