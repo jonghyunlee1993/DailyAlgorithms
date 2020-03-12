e27e2b2a7de8a20162318d3e4a8a5716a8f10c7e
Jonghyun Lee <green198800@yonsei.ac.kr>
2020-03-12 23:22:06 +0900

    python quiz

--- Python/progrmmers/후보키.py
+++ Python/progrmmers/후보키.py
@@ -1,3 +1,31 @@
+# 비트 연산자 &를 이용해서 최소성 체크가 핵심
+
+import pandas as pd
+from itertools import combinations
+
+def solution(relation):
+    answer = []
+    df = pd.DataFrame(relation)
+    
+    for i in range(1, len(df.columns) + 1):
+        candidates = list(combinations(set(df.columns), i))
+
+        for columns in candidates:
+            temp = df[list(columns)].apply("".join, axis=1).unique()
+            
+            if len(temp) == len(df):
+                is_exist = False
+                
+                for check in answer:
+                    if check & set(list(columns)) == check:
+                        is_exist = True
+                        break
+
+                if is_exist == False:
+                    answer.append(set(list(columns)))
+                    
+    return len(answer)
+
 # 2개를 최소성이라고 가정하고 실시하였는데, 문제를 제대로 이해하지 못한 듯 함 
 
 import pandas as pd
