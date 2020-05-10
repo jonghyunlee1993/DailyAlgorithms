ac0726fbbd00a4c6c862d2e38b69f543485192af
Jonghyun Lee <green198800@yonsei.ac.kr>
2020-05-10 11:18:25 +0900

    Add pyhton quiz

--- Python/progrmmers/베스트앨범.py
+++ Python/progrmmers/베스트앨범.py
@@ -1,20 +1,19 @@
-# pandas를 이용해서 풀고 싶으나
-# 런타임 오류가 발생하는 문제가 있음
-
 import pandas as pd
 
 def solution(genres, plays):
     data = pd.DataFrame({"genres": genres, "plays":plays})
-    data = data.sort_values(by=["plays", "genres"], axis=0, ascending=False)
+    data = data.sort_values(by=["genres", "plays"], axis=0, ascending=False)
+    popular_genres = data.groupby(['genres']).apply(lambda x: x.plays.sum()).sort_values(ascending=False).index.to_list()
     
     answer = []
     
-    for genre in pd.unique(data['genres'].to_list()):
+    for genre in popular_genres:
         genre_data = data[data['genres'] == genre].iloc[:2]
         music_index = genre_data.index.to_list()
         
-        if genre_data['plays'].iloc[0] == genre_data['plays'].iloc[1]:
-            music_index = sorted(music_index)
+        if len(genre_data) >= 2:
+            if genre_data['plays'].iloc[0] == genre_data['plays'].iloc[1]:
+                music_index = sorted(music_index)
         
         answer.extend(music_index)
         
